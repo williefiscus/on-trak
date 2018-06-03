b239f68378f6bc60a0eceb01b60790e175292b7f
Angelo Amon <angelo.amon416@live.com>
2018-06-02 19:17:07 -0700

    Added Roles and ability to create, edit and add users to each roles.

---
+++ OnTrak/Controllers/AccountController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OnTrak.Models.Entities.User;
+using OnTrak.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnTrak.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private UserManager<AppUser> userManager;
+        private SignInManager<AppUser> signInManager;
+
+        public AccountController(UserManager<AppUser> usrMgr, SignInManager<AppUser> signinMgr) {
+            userManager = usrMgr;
+            signInManager = signinMgr;
+        }
+
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel details, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = await userManager.FindByEmailAsync(details.Email);
+                if (user != null)
+                {
+                    await signInManager.SignOutAsync();
+                    Microsoft.AspNetCore.Identity.SignInResult result = await signInManager.PasswordSignInAsync(user, details.Password, false, false);
+
+                    if (result.Succeeded)
+                    {
+                        return Redirect(returnUrl ?? "/");
+                    }
+                }
+
+                ModelState.AddModelError(nameof(LoginViewModel.Email), "Invalid user or password");
+            }
+            return View(details);
+        }
+    }
+}
